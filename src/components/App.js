aa6f4bdd220078f41892a9706dfac8661c0ffa85
Prateek Jaiswal <prateekjais.dev@gmail.com>
2020-02-23 05:11:13 +0530

    Fixed this binding in Class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar></SearchBar>
-    </div>
-  );
-};
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,25 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { term: '' };
+    // this.onFormSubmit = this.onFormSubmit.bind(this); USE THIS WITH (A)
   }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  //   onFormSubmit(e) { //THIS IS (A)
+  //     e.preventDefault();
+  //     this.props.onSubmit(this.state.term);
+  //   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        {/* <form className="ui form" onSubmit={this.onFormSubmit}> */}
+        {/* <form className="ui form" onSubmit={e => this.onFormSubmit(e)}> */}
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>
               <p>Search Bar</p>
